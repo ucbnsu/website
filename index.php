3eb8f033946200145677abda7ed81be2d10516ab
Takeshi Mochida <tmochida@ocf.berkeley.edu>
2017-02-08 13:35:36 -0800

    Use header and footer files
    No more duplicate code

--- index.php
+++ index.php
@@ -1,162 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Nikkei Student Union</title>
+        <?php include 'pages/head.php' ?>
         <link rel="icon" href="/favicon.ico" type="image/x-icon">
-        <!-- Bootstrap -->
-        <link href="css/bootstrap.min.css" rel="stylesheet">
-        <link rel="stylesheet" type="text/css" href="css/style.css"> 
-        <link href='https://fonts.googleapis.com/css?family=Carrois+Gothic' rel='stylesheet' type='text/css'>
-
-        <!-- 1. Link to jQuery (1.8 or later), -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script> <!-- 33 KB -->
-
-        <script src="js/hamburger.js"></script>
-        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-        <!--[if lt IE 9]>
-            <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-            <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-        <![endif]-->
     </head>
     <body>
+        <?php include 'pages/header.php' ?>
         <?php include_once("pages/analyticstracking.php") ?>
-        <div id="header" class="green row text-center main-nav">
-            <center>
-                <div class="col-sm-4 col-sm-offset-1 height_40">
-                    <span id="facebook" class="span_icon">
-                            <a href="https://www.facebook.com/groups/2200207758/">
-                                <img src="pictures/facebook_icon.png" alt="Facebook Logo">
-                            </a>
-                    </span>
-                    <!--
-                    <span id="twitter" class="span_icon">
-                        <a href="https://twitter.com/BerkeleyNSU">
-                            <img src="pictures/Twitter_icon.png" alt="">
-                        </a>
-                    </span>
-                    <span id="tumblr" class="span_icon">
-                        <a href="http://berkeleynsu.tumblr.com/">
-                            <img src="pictures/Tumblr_icon.png" alt="Tumblr Icon">
-                        </a>
-                    </span>
-                    <span id="instagram" class="span_icon">
-                        <a href="http://instagram.com/berkeleynsu"ƒ>
-                            <img src="pictures/instagram_icon.png" alt="Instagram Icon">
-                        </a>
-                    </span>
-                    -->
-                    <span id="youtube" class="span_icon">
-                        <a href="https://www.youtube.com/user/NSUBerkeley">
-                            <img src="pictures/youtube_icon.png" alt="Youtube Icon">
-                        </a>
-                    </span>
-                    <span id="smugmug" class="span_icon">
-                        <a href="http://ucb-nsu.smugmug.com/">
-                            <img src="pictures/smugmug_icon.png" alt="Smugmug Icon">
-                        </a>
-                    </span>
-                    <span id="Contact">
-                        <a class="hover_link" href="pages/Contact.php">
-                            Contact
-                        </a>
-                    </span>
-                </div>
-                <div class="col-sm-1 col-sm-offset-1 text-center">
-                </div>
-                <div class="col-sm-4 push_apart_15">
-                    <span id="About">
-                        <a class="hover_link" href="pages/About.php">
-                            About
-                        </a>
-                    </span>
-                    <span id="Calendar">
-                        <a class="hover_link" href="pages/Calendar.php">
-                            Calendar
-                        </a>
-                    </span>
-                </div>
-            </center>
-        </div>
-        <div id="mobile-div" class="row">
-            <div class="container">
-                <div class="col-sm-12 mobile-nav">
-                        <div class="menu-btn" id="menu-btn">
-                            <div></div>
-                            <span></span>
-                            <span></span>
-                            <span></span>
-                         </div>
-                     
-                         <div class="responsive-menu">
-                            <ul>
-                                <li>
-                                    <span id="facebook" class="span_icon">
-                                    <a href="https://www.facebook.com/groups/2200207758/">
-                                        <img src="pictures/facebook_icon.png" alt="Facebook Logo">
-                                        Facebook
-                                    </a>
-                                    </span>
-                                </li>
-                                <li>
-                                    <span id="smugmug" class="span_icon">
-                                        <a href="http://ucb-nsu.smugmug.com/">
-                                            <img src="pictures/smugmug_icon.png" alt="Smugmug Icon">
-                                            Smugmug
-                                        </a>
-                                    </span>
-                                </li>
-                                <li>
-                                    <span id="Contact">
-                                        <a class="hover_link" href="pages/Contact.php">
-                                            Contact Us
-                                        </a>
-                                    </span>
-                                </li>
-                               <li>
-                                    <a class="hover_link" href="pages/About.php">
-                                        About Us
-                                    </a>
-                                        </li>
-                               <li>
-                                    <a class="hover_link" href="pages/Calendar.php">
-                                        Calendar
-                                    </a>
-                                </li>
-                                <li>
-                                    <a class="hover_link" href="pages/CultureShow.php">Culture Show</a>
-                                </li>
-                                <li>
-                                    <a class="hover_link" href="pages/Core.php">Core</a>
-                                </li>
-                                <li>
-                                    <a class="hover_link" href="pages/Yosakoi.php">Yosakoi</a>
-                                </li>
-                                <li>
-                                    <a class="hover_link" href="pages/NiCE.php">Nikkei Choral Ensemble[NiCE]</a>
-                                </li>
-                            </ul>
-                         </div>
-                </div>
-            </div>
-        </div>
+
         <div class="row">
             <div class="container">
                 <img id="homepage_image" src="pictures/homepage_image.png" alt="">
-            </div>
-        </div>
-        <div id="middle_nav" class="green nav row main-nav">
-            <div class="push_down">
-                <div class="text-center">
-                    <a class="hover_link" href="pages/CultureShow.php">Culture Show</a>
-                    <!--<a class="hover_link" href="pages/Remembrance">Day of Remembrance</a> -->
-                    <a class="hover_link" href="pages/Core.php">Core</a>
-                    <a class="hover_link" href="pages/Yosakoi.php">Yosakoi</a>
-                    <a class="hover_link" href="pages/NiCE.php">Nikkei Choral Ensemble[NiCE]</a>
-                </div>
             </div>
         </div>
         <div class="row">
@@ -227,59 +81,7 @@
                 </div>
             </div>
         </div>
-        <div class="footer green">
-            <div class="col-sm-1 col-sm-offset-1 green">
-                <h2>Other Links</h2>
-                <h6 id="copyright">&copy; UC Berkeley Nikkei Student Union. We are a student group acting independently of the University of California. We take full responsibility for our organization and this web site.
- <?php echo date('Y'); ?></h6>
-		<a href="https://www.ocf.berkeley.edu">
-    <img src="https://www.ocf.berkeley.edu/hosting-logos/ocfbadge_mini8.png"
-        alt="Hosted by the OCF" style="border: 0;" />
-</a>
-            </div>
-            <div class="col-sm-3 green">
-                <h2>Related Campus Orgs</h2>
-                <a class="footer_link hover_link" href="http://taiko.berkeley.edu/" target="_blank">Cal Raijin Taiko</a><br>
-                <a class="footer_link hover_link" href="http://www.ocf.berkeley.edu/~cjc/" target="_blank">Cal Japan Club</a><br>
-                <a class="footer_link hover_link" href="http://calorigami.weebly.com/" target="_blank">Cal Origami</a><br>
-                <a class="footer_link hover_link" href="http://kendo.berkeley.edu/" target="_blank">Berkeley Kendo Club</a><br>
-                <a class="footer_link hover_link" href="http://msa.berkeley.edu/site/" target="_blank">Muslim Student Association</a>
-                <h2>Educational Sites </h2>
-                <a class="footer_link hover_link" href="http://www.njahs.org/" target="_blank">National Japanese American Historical Society</a><br>
-                <a class="footer_link hover_link" href="http://www.janm.org/" target="_blank">Japanese American National Museum</a><br>
-                <a class="footer_link hover_link" href="http://www.goforbroke.org/" target="_blank">Go For Broke National Education Center</a><br>
-                <a class="footer_link hover_link" href="http://www.ltsc.org/" target="_blank">Little Tokyo Service Center</a><br>
-            </div>
-            <div class="col-sm-4 green">
-                <h2> Other Nikkei Student Orgs </h2>
-                <div class="col-sm-6 green">
-                    <a class="footer_link hover_link" href="http://jassorg.wordpress.com/" target="_blank">UC Davis JASS</a><br>
-                    <a class="footer_link hover_link" href="http://www.clubs.uci.edu/tomo/ " target="_blank">UC Irvine Tomo no Kai</a><br>
-                    <a class="footer_link hover_link" href="http://www.nikkeibruins.org/ " target="_blank">UCLA Nikkei Student Union</a><br>
-                    <a class="footer_link hover_link" href="http://www.ucsdnsu.com/ " target="_blank">UC San Diego Nikkei Student Union</a><br>
-                    <a class="footer_link hover_link" href="http://www.facebook.com/home?sk=group_117317368325408 " target="_blank">CSUF Nikkei Student Union</a><br>
-                    <a class="footer_link hover_link" href="http://sfsujsa.blog126.fc2.com/ " target="_blank">SFSU Japanese Student Association</a><br>
-                    <a class="footer_link hover_link" href="http://www.csulb.edu/org/cultural/nikkei/" target="_blank">CSULB Nikkei Student Union</a><br>
-                    <a class="footer_link hover_link" href="http://www-scf.usc.edu/~nikkei/" target="_blank">USC Nikkei Student Union</a><br>
-                </div>
-                <div class="col-sm-6 green">
-                    <a class="footer_link hover_link" href="http://www.facebook.com/group?gid=2200245595" target="_blank">UCSB Nikkei Student Union</a><br>
-                    <a class="footer_link hover_link" href="http://www.facebook.com/group?gid=104717296236429" target="_blank">SDSU Nikkei Student Union</a><br>
-                    <a class="footer_link hover_link" href="http://jsaucsc.wordpress.com/" target="_blank">UCSC Japanese Student Association</a><br>
-                    <a class="footer_link hover_link" href="http://www.csun.edu/~csunnsa/home.html" target="_blank">CSUN Nikkei Student Association</a><br>
-                    <a class="footer_link hover_link" href="http://cppnsu.com/" target="_blank">Cal Poly Pomona Nikkei Student Union</a><br>
-                </div>
-            </div>
-            <div class="col-sm-2 green">
-                <h2>Community Organizations</h2>
-                <a class="footer_link hover_link" href="http://ww/www.j-sei.org/" target="_blank">J-Sei</a><br>
-                <a class="footer_link hover_link" href="http://www.jcccnc.org/" target="_blank">Japanese Cultural and Community Center of Northern California (JCCCNC) </a><br>
-                <a class="footer_link hover_link" href="http://www.jcyc.org/" target="_blank">Japanese Community Youth Council (JCYC)</a><br>
-                <a class="footer_link hover_link" href="http://www.jacl.org/" target="_blank">Japanese American Citizens League (JACL)</a><br>
-                <a class="footer_link hover_link" href="http://nikkeiyouth.org/" target="_blank">Nikkei Youth</a><br>
-                <a class="footer_link hover_link" href="http://www.cjaaa.org/" target="_blank">California Japanese American Alumni Association (CJAAA)</a><br>
-                <a class="footer_link hover_link" href="http://korematsuinstitute.org/" target= "_blank">Fred T. Korematsu Institute for Civil Rights and Education</a>
-            </div>
-        </div>
+
+        <?php include 'pages/footer.php' ?>
     </body>
 </html>
