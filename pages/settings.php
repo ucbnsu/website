a2af4ae4f780d1b4b016fdda7ff2293dd6018e6a
Takeshi Mochida <tmochida@ocf.berkeley.edu>
2017-02-24 00:15:41 -0800

    Set site-wide default timezone

--- pages/settings.php
+++ pages/settings.php
@@ -1,6 +1,8 @@
 <?php
-// Loads site-wide settings stored in INI file
+// Set timezone to PST
+date_default_timezone_set('America/Los_Angeles');
 
+// Load site-wide settings stored in INI file
 define("CONFIG", dirname(__FILE__) . "/../config.ini");
 $ini_array = parse_ini_file(CONFIG, true);
 ?>
