dd1234d5bd149f46fde0b177ad11e9e583dc2998
Takeshi Mochida <tmochida@ocf.berkeley.edu>
2017-02-24 21:56:03 -0800

    Clean up footer

--- pages/footer.php
+++ pages/footer.php
@@ -1,9 +1,17 @@
-		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-	    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-	    <!-- Include all compiled plugins (below), or include individual files as needed -->
-	    <script src="/js/bootstrap.min.js"></script>
-		<div class="footer green">
-			<div class="col-md-2 col-md-offset-1 text-center green">
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="/js/bootstrap.min.js"></script>
+<div class="footer green">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <a id="contact" href="Contact.php">
+                    Contact
+                </a>
+            </div>
+
+			<div class="col-md-4 green">
 				<h2>Connect with us</h2>
 				<span id="facebook" class="span_icon">
 					<a href="https://www.facebook.com/groups/2200207758/">
@@ -35,54 +43,11 @@
 						<img src="/pictures/smugmug_icon.png" alt="Smugmug Icon">
 					</a>
 				</span>
-				<div class="col-md-12">
-						<a id="contact" href="Contact.php">
-							Contact
-						</a>
-				</div>
-				<h6 id="copyright">&copy; UC Berkeley Nikkei Student Union 2017</h6>
 			</div>
-			<div class="col-md-2 green">
-				<h2>Related Campus Orgs</h2>
-				<a class="footer_link hover_link" href="http://taiko.berkeley.edu/" target="_blank">Cal Raijin Taiko</a><br>
-				<a class="footer_link hover_link" href="http://www.ocf.berkeley.edu/~cjc/" target="_blank">Cal Japan Club</a><br>
-				<a class="footer_link hover_link" href="http://calorigami.weebly.com/" target="_blank">Cal Origami</a><br>
-				<a class="footer_link hover_link" href="http://kendo.berkeley.edu/" target="_blank">Berkeley Kendo Club</a><br>
-				<a class="footer_link hover_link" href="http://msa.berkeley.edu/site/" target="_blank">Muslim Student Association</a>
-				<h2>Educational Sites </h2>
-				<a class="footer_link hover_link" href="http://www.njahs.org/" target="_blank">National Japanese American Historical Society</a><br>
-				<a class="footer_link hover_link" href="http://www.janm.org/" target="_blank">Japanese American National Museum</a><br>
-				<a class="footer_link hover_link" href="http://www.goforbroke.org/" target="_blank">Go For Broke National Education Center</a><br>
-				<a class="footer_link hover_link" href="http://www.ltsc.org/" target="_blank">Little Tokyo Service Center</a><br>
-			</div>
-			<div class="col-md-4 green">
-				<h2> Other Nikkei Student Orgs </h2>
-				<div class="col-md-6">
-					<a class="footer_link hover_link" href="http://jassorg.wordpress.com/" target="_blank">UC Davis JASS</a><br>
-					<a class="footer_link hover_link" href="http://www.clubs.uci.edu/tomo/ " target="_blank">UC Irvine Tomo no Kai</a><br>
-					<a class="footer_link hover_link" href="http://www.nikkeibruins.org/ " target="_blank">UCLA Nikkei Student Union</a><br>
-					<a class="footer_link hover_link" href="http://www.ucsdnsu.com/ " target="_blank">UC San Diego Nikkei Student Union</a><br>
-					<a class="footer_link hover_link" href="http://www.facebook.com/home.php?sk=group_117317368325408 " target="_blank">CSUF Nikkei Student Union</a><br>
-					<a class="footer_link hover_link" href="http://sfsujsa.blog126.fc2.com/ " target="_blank">SFSU Japanese Student Association</a><br>
-					<a class="footer_link hover_link" href="http://www.csulb.edu/org/cultural/nikkei/" target="_blank">CSULB Nikkei Student Union</a><br>
-					<a class="footer_link hover_link" href="http://www-scf.usc.edu/~nikkei/" target="_blank">USC Nikkei Student Union</a><br>
-				</div>
-				<div class="col-md-6">
-					<a class="footer_link hover_link" href="http://www.facebook.com/group.php?gid=2200245595" target="_blank">UCSB Nikkei Student Union</a><br>
-					<a class="footer_link hover_link" href="http://www.facebook.com/group.php?gid=104717296236429" target="_blank">SDSU Nikkei Student Union</a><br>
-					<a class="footer_link hover_link" href="http://jsaucsc.wordpress.com/" target="_blank">UCSC Japanese Student Association</a><br>
-					<a class="footer_link hover_link" href="http://www.csun.edu/~csunnsa/home.html" target="_blank">CSUN Nikkei Student Association</a><br>
-					<a class="footer_link hover_link" href="http://cppnsu.com/" target="_blank">Cal Poly Pomona Nikkei Student Union</a><br>
-				</div>
-			</div>
-			<div class="col-md-2 green">
-				<h2>Community Organizations</h2>
-				<a class="footer_link hover_link" href="http://ww/www.j-sei.org/" target="_blank">J-Sei</a><br>
-				<a class="footer_link hover_link" href="http://www.jcccnc.org/" target="_blank">Japanese Cultural and Community Center of Northern California (JCCCNC) </a><br>
-				<a class="footer_link hover_link" href="http://www.jcyc.org/" target="_blank">Japanese Community Youth Council (JCYC)</a><br>
-				<a class="footer_link hover_link" href="http://www.jacl.org/" target="_blank">Japanese American Citizens League (JACL)</a><br>
-				<a class="footer_link hover_link" href="http://nikkeiyouth.org/" target="_blank">Nikkei Youth</a><br>
-				<a class="footer_link hover_link" href="http://www.cjaaa.org/" target="_blank">California Japanese American Alumni Association (CJAAA)</a><br>
-				<a class="footer_link hover_link" href="http://korematsuinstitute.org/" target= "_blank">Fred T. Korematsu Institute for Civil Rights and Education</a>
-			</div>
-		</div>
+
+            <div class="col-md-4 green">
+                <h2><a class="hover_link" href="/pages/external_links.php">External Links</a></h2>
+            </div>
+        </div>
+    </div>
+</div>
