96fbbc45e455a4fb51d7d230360298b60332dd55
Takeshi Mochida <tmochida@ocf.berkeley.edu>
2017-02-27 16:20:20 -0800

    Add OCF banner, remove tab characters

--- pages/footer.php
+++ pages/footer.php
@@ -19,43 +19,52 @@
                 </div>
             </div>
 
-			<div class="col-md-4 green">
-				<h2>Connect with us</h2>
-				<span id="facebook" class="span_icon">
-					<a href="https://www.facebook.com/groups/2200207758/">
-						<img src="/pictures/facebook_icon.png" alt="Facebook Logo">
-					</a>
-				</span>
-				<span id="twitter" class="span_icon">
-					<a href="https://twitter.com/BerkeleyNSU">
-						<img src="/pictures/Twitter_icon.png" alt="">
-					</a>
-				</span>
-				<span id="tumblr" class="span_icon">
-					<a href="http://berkeleynsu.tumblr.com/">
-						<img src="/pictures/Tumblr_icon.png" alt="Tumblr Icon">
-					</a>
-				</span>
-				<span id="instagram" class="span_icon">
-					<a href="http://instagram.com/berkeleynsu"ƒ>
-						<img src="/pictures/instagram_icon.png" alt="Instagram Icon">
-					</a>
-				</span>
-				<span id="youtube" class="span_icon">
-					<a href="https://www.youtube.com/user/NSUBerkeley">
-						<img src="/pictures/youtube_icon.png" alt="Youtube Icon">
-					</a>
-				</span>
-				<span id="smugmug" class="span_icon">
-					<a href="http://ucb-nsu.smugmug.com/">
-						<img src="/pictures/smugmug_icon.png" alt="Smugmug Icon">
-					</a>
-				</span>
-			</div>
-
+            <div class="col-md-4 green">
+                <h2>Connect with us</h2>
+                <span id="facebook" class="span_icon">
+                    <a href="https://www.facebook.com/groups/2200207758/">
+                        <img src="/pictures/facebook_icon.png" alt="Facebook Logo">
+                    </a>
+                </span>
+                <span id="twitter" class="span_icon">
+                    <a href="https://twitter.com/BerkeleyNSU">
+                        <img src="/pictures/Twitter_icon.png" alt="">
+                    </a>
+                </span>
+                <span id="tumblr" class="span_icon">
+                    <a href="http://berkeleynsu.tumblr.com/">
+                        <img src="/pictures/Tumblr_icon.png" alt="Tumblr Icon">
+                    </a>
+                </span>
+                <span id="instagram" class="span_icon">
+                    <a href="http://instagram.com/berkeleynsu"ƒ>
+                        <img src="/pictures/instagram_icon.png" alt="Instagram Icon">
+                    </a>
+                </span>
+                <span id="youtube" class="span_icon">
+                    <a href="https://www.youtube.com/user/NSUBerkeley">
+                        <img src="/pictures/youtube_icon.png" alt="Youtube Icon">
+                    </a>
+                </span>
+                <span id="smugmug" class="span_icon">
+                    <a href="http://ucb-nsu.smugmug.com/">
+                        <img src="/pictures/smugmug_icon.png" alt="Smugmug Icon">
+                    </a>
+                </span>
+                <br />
+            </div>
             <div class="col-md-4 green">
                 <h2><a class="hover_link" href="/pages/external_links.php">External Links</a></h2>
             </div>
+        </div>
+
+        <div class="row" style="color:white">
+            <br />
+            <h6 id="copyright">&copy; UC Berkeley Nikkei Student Union 2017</h6>
+            <h6>We are a student group acting independently of the University of California. We take full responsibility for our organization and this web site.</h6>
+            <a href="https://www.ocf.berkeley.edu">
+                <img src="https://www.ocf.berkeley.edu/hosting-logos/ocfbadge_mini8.png" alt="Hosted by the OCF" style="border: 0;" />
+            </a>
         </div>
     </div>
 </div>
