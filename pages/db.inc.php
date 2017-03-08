002a26faeac916955f76259a534be84a48edeb75
Takeshi Mochida <tmochida@ocf.berkeley.edu>
2017-03-08 15:51:54 -0800

    Serve gallery information from database

--- pages/db.inc.php
+++ pages/db.inc.php
@@ -1,11 +1,16 @@
 <?php
-include 'settings.php';
+require_once 'settings.php';
+
 $dbinfo = $ini_array['db'];
 $host = $dbinfo['hostname'];
 $user = $dbinfo['username'];
 $pass = $dbinfo['password'];
 $dtbs = $dbinfo['database'];
 
-$connection = mysql_connect($host, $user, $pass);
-mysql_select_db($dtbs);
+$mysqli = new mysqli($host, $user, $pass, $dtbs);
+
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: " . $mysqli->connect_error;
+}
+
 ?>
--- pages/gallery.php
+++ pages/gallery.php
@@ -1,71 +1,20 @@
 <?php
 require_once 'settings.php';
 require_once 'facebook_api.php';
-
-$eventsinfo = $ini_array['events'];
-if (isset($eventsinfo['additionalEvents'])) {
-    $additionalEvents = $eventsinfo['additionalEvents'];
-} else {
-    $additionalEvents = array();
-}
+require_once 'database.php';
 
 $fb_gid = $fbinfo['groupid'];
 $fb_token = $fbinfo['token'];
 
-$eventsinfo = $ini_array['events'];
 $ev_options = [
 ];
 
 $req_method = 'GET';
 $req_endpoint = '/' . $fb_gid . '/albums';
 
-//$response = $fb->sendRequest($req_method, $req_endpoint, $ev_options, $fb_token);
-//$event_arr_tmp = $response->getDecodedBody();
-$response = $fb->sendRequest($req_method, $req_endpoint, $ev_options, $fb_token)->getDecodedBody();
-$albums = array();
-if (isset($response['data'])) {
-    $albums = $response['data'];
-}
-//var_dump($albums);
 ?>
 
 <?php
-// TODO move functions into api file
-// TODO investigate if pagination required if more albums exist
-function getAlbumCoverSrc($albumId) {
-    global $fb, $fb_token;
-    $method = 'GET';
-    $endpoint = '/' . $albumId;
-    $options = [
-        'fields' => 'cover_photo',
-    ];
-
-    // Get ID of cover photo
-    $response = $fb->sendRequest($method, $endpoint, $options, $fb_token)->getDecodedBody();
-    $coverId = $response['cover_photo']['id'];
-
-    $endpoint = '/' . $coverId;
-    $options = [
-        'fields' => 'images',
-    ];
-
-    // Get images for cover photo, returned as a list.
-    $response = $fb->sendRequest($method, $endpoint, $options, $fb_token)->getDecodedBody();
-    $images = $response['images'];
-
-    if (count($images) < 1) {
-        return ""; // TODO come up with default cover
-    }
-
-    // Select a reasonably sized image out of the list, otherwise return first image.
-    foreach ($images as $image) {
-        if ($image['width'] < 500) {
-            return $image['source'];
-        }
-    }
-    return $images[0]['source'];
-}
-
 function getAlbumLink($albumId) {
     $link = 'https://www.facebook.com/media/set/?set=oa.';
     return $link . $albumId . '&type=3';
@@ -73,10 +22,10 @@ function getAlbumLink($albumId) {
 
 function albumHtml($album) {
     $albumUrl = getAlbumLink($album['id']);
-    $coverUrl = getAlbumCoverSrc($album['id']);
+    $coverUrl = $album['cover_url'];
 
     echo '<div class="col-md-4">';
-    echo "<h3>{$album['name']}</h3>";
+    echo "<h3>{$album['title']}</h3>";
     echo "<img style=\"width:100%;\" src=\"{$coverUrl}\" ><br />";
     echo "<p class=\"text-right\"><a style=\"color:black\" href=\"{$albumUrl}\">Go to Album</a></p>";
     echo "</div>";
@@ -84,16 +33,18 @@ function albumHtml($album) {
 
 function displayRow($row) {
     echo '<div class="row">';
-    foreach ($row as $item) {
-        albumHtml($item);
+    foreach ($row as $album) {
+        albumHtml($album);
     }
     echo '</div>';
 }
 
-function displayAlbums($arr) {
+function displayAlbums() {
+    global $mysqli;
+    $albums = loadGalleryFromDb($mysqli);
     $i = 0;
-    while ($i < count($arr)) {
-        $row = array_slice($arr, $i, min(3, count($arr) - $i));
+    while ($i < count($albums)) {
+        $row = array_slice($albums, $i, min(3, count($albums) - $i));
         displayRow($row);
         $i += 3;
     }
@@ -113,7 +64,7 @@ function displayAlbums($arr) {
             </div>
         </div>
 
-        <?php displayAlbums($albums); ?>
+        <?php displayAlbums(); ?>
 
     </div>
     <?php include 'footer.php' ?>
