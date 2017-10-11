1edefad1bfc4695e6b2553648e59870bc0135f99
Takeshi Mochida <tmochida@ocf.berkeley.edu>
2017-10-11 00:15:31 -0700

    External links: update/remove old links

--- pages/external_links.php
+++ pages/external_links.php
@@ -14,7 +14,7 @@
 				<a style="color:black" href="http://cjc.berkeley.edu/" target="_blank">Cal Japan Club</a><br>
 				<a style="color:black" href="http://calorigami.berkeley.edu/" target="_blank">Cal Origami</a><br>
 				<a style="color:black" href="http://kendo.berkeley.edu/" target="_blank">Berkeley Kendo Club</a><br>
-				<a style="color:black" href="http://msa.berkeley.edu/site/" target="_blank">Muslim Student Association</a>
+				<a style="color:black" href="http://www.berkeleymsa.com/" target="_blank">Muslim Student Association</a>
             </div>
             <div class="col-md-6">
                 <h1>Community Organizations</h1>
@@ -23,7 +23,6 @@
                 <a style="color:black" href="http://www.jcyc.org/" target="_blank">Japanese Community Youth Council (JCYC)</a><br>
                 <a style="color:black" href="http://www.jacl.org/" target="_blank">Japanese American Citizens League (JACL)</a><br>
                 <a style="color:black" href="http://nikkeiyouth.org/" target="_blank">Nikkei Youth</a><br>
-                <a style="color:black" href="http://www.cjaaa.org/" target="_blank">California Japanese American Alumni Association (CJAAA)</a><br>
                 <a style="color:black" href="http://korematsuinstitute.org/" target= "_blank">Fred T. Korematsu Institute for Civil Rights and Education</a>
             </div>
         </div>
@@ -38,18 +37,13 @@
             <div class="col-md-6">
                 <h1> Other Nikkei Student Orgs </h1>
                 <a style="color:black" href="http://jassorg.wordpress.com/" target="_blank">UC Davis JASS</a><br>
-                <a style="color:black" href="http://www.clubs.uci.edu/tomo/ " target="_blank">UC Irvine Tomo no Kai</a><br>
-                <a style="color:black" href="http://www.nikkeibruins.org/ " target="_blank">UCLA Nikkei Student Union</a><br>
-                <a style="color:black" href="http://www.ucsdnsu.com/ " target="_blank">UC San Diego Nikkei Student Union</a><br>
-                <a style="color:black" href="http://www.facebook.com/home.php?sk=group_117317368325408 " target="_blank">CSUF Nikkei Student Union</a><br>
-                <a style="color:black" href="http://sfsujsa.blog126.fc2.com/ " target="_blank">SFSU Japanese Student Association</a><br>
+                <a style="color:black" href="http://www.ucitomonokai.com/" target="_blank">UC Irvine Tomo no Kai</a><br>
+                <a style="color:black" href="https://www.facebook.com/uclanikkei/" target="_blank">UCLA Nikkei Student Union</a><br>
+                <a style="color:black" href="http://ucsdnsu.weebly.com/" target="_blank">UC San Diego Nikkei Student Union</a><br>
+                <a style="color:black" href="https://www.facebook.com/groups/117317368325408/" target="_blank">CSUF Nikkei Student Union</a><br>
                 <a style="color:black" href="http://www.csulb.edu/org/cultural/nikkei/" target="_blank">CSULB Nikkei Student Union</a><br>
-                <a style="color:black" href="http://www-scf.usc.edu/~nikkei/" target="_blank">USC Nikkei Student Union</a><br>
-                <a style="color:black" href="http://www.facebook.com/group.php?gid=2200245595" target="_blank">UCSB Nikkei Student Union</a><br>
-                <a style="color:black" href="http://www.facebook.com/group.php?gid=104717296236429" target="_blank">SDSU Nikkei Student Union</a><br>
-                <a style="color:black" href="http://jsaucsc.wordpress.com/" target="_blank">UCSC Japanese Student Association</a><br>
-                <a style="color:black" href="http://www.csun.edu/~csunnsa/home.html" target="_blank">CSUN Nikkei Student Association</a><br>
-                <a style="color:black" href="http://cppnsu.com/" target="_blank">Cal Poly Pomona Nikkei Student Union</a><br>
+                <a style="color:black" href="https://jsaucsc.wordpress.com" target="_blank">UCSC Japanese Student Association</a><br>
+                <a style="color:black" href="https://www.facebook.com/groups/CPPNSU/" target="_blank">Cal Poly Pomona Nikkei Student Union</a><br>
             </div>
         </div>
     </div>
